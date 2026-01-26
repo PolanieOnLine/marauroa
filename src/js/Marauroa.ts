a6be5d29fb919ccdee699b8bcfc92d0b102e86b7
KarajuSs <35039330+KarajuSs@users.noreply.github.com>
2026-01-26 13:15:28 +0100

    created a Marauroa class for the marauroa global object

---
+++ src/js/Marauroa.ts
@@ -0,0 +1,16 @@
+class Marauroa {
+	debug: {
+		messages: false,
+		unknownEvents: true
+	}
+	currentZone: any;
+	perceptionHandler: any;
+	perceptionListener: any;
+	currentZoneName: string;
+	rpobjectFactory: any;
+	rpslotFactory: any;
+	rpeventFactory: any;
+	me: any;
+}
+
+export const marauroa = new Marauroa();
